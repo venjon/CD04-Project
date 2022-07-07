e2cfb7afe58e7b9adba6bc9d369a4a3a43325412
Vennela <vennelashriya@gmail.com>
2022-07-07 10:36:12 -0700

    Update FirstWindow.py

--- FirstWindow.py
+++ FirstWindow.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import filedialog, Text
 import os
 root = tk.Tk()
+
 canvas = tk.Canvas(root, height = 700, width = 700, bg="#263D42")
 canvas.pack()
 
@@ -9,6 +10,7 @@ frame = tk.Frame(root, bg="white")
 frame.place(relwidth = 0.8, relheight = 0.8, relx = 0.1, rely = 0.1)
 
 openFile = tk.Button(frame, text ="Open File", padx = 10, pady = 5, 
-                      bg ="#263D42", fg = "black") 
+                      bg ="white", fg = "black") 
 openFile.pack()
 
+root.mainloop()
