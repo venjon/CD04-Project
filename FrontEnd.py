cb9a4df4067789bc0352c4e8aa09250e1f002bb7
tharukakk1 <tharukakk1@gmail.com>
2022-07-08 17:08:15 -0700

    Two-Panel Design, Title Entry Bar, Rocks Entry Bar (incomplete)

--- FrontEnd.py
+++ FrontEnd.py
@@ -1,21 +1,51 @@
 import tkinter as tk
-from tkinter import filedialog, Text, messagebox
+from tkinter import BOTH, Y, filedialog, Text, messagebox
 import os
+from tkinter.ttk import PanedWindow
 root = tk.Tk()
-canvas = tk.Canvas(root, width=800, height=800, bg="#f0f0f0")
-canvas.pack()
+lcanvas = tk.Canvas(root, width=400, height=800, bg="#f0f0f0")
+lcanvas.pack(side="left")
+rcanvas = tk.Canvas(root, width=400, height=800, bg="white",)
+rcanvas.pack(side="right")
 
-#Name of Sample
-a = tk.Label(root, text="Name of Sample (5 characters)")
-canvas.create_window(300, 100, window=a)
+#Left-Side Menus
+#TITLE
+titleLabel = tk.Label(lcanvas, text="Document Title (max 80 characters):").place(x=10, y=10)
+var0 = tk.StringVar()
+titleEntry = tk.Entry(lcanvas, textvariable=var0)
+titleEntry.place(x=207,y=10)
+title = titleEntry.get()
 
-rockName = tk.Entry(root)
-canvas.create_window(500,100,window=rockName)
+#ROCKS
+def RocksOptionMenu_SelectionEvent(event):
+    RockChoice = var1.get()
+    if RockChoice == "Add Material":
+        print("good")
+        addMaterial()
+    else:
+        print("bad")
+    pass
 
-def rockNameCheck():
-    rockNameLetters = list(rockName)
-    if len(rockNameLetters) != 5:
-        tk.messagebox.showerror(message="Sample name must be 5 letters long!")
-        
+def addMaterial():
+    print("hello world")
+    newMat = tk.Toplevel(root)
+    newMat.geometry("200x100")
+    tk.Label(newMat,text="""Enter a name for your new sample
+    (preferably 5 characters)""").pack()
+    newMatName0 = tk.StringVar()
+    newMatName = tk.Entry(newMat, width=30, textvariable=newMatName0).pack()
+    options.append(newMatName0.get())
+    def var3():
+        print("hello world")
+        rockList.set('')
+    var2=tk.Button(newMat, text="Enter", command=lambda:var3())
+    var2.pack()
+    tk.Label(lcanvas, text=options).pack()
+
+var1 = tk.StringVar()
+var1.set("Materials")
+options = ["Add Material"]
+rockList = tk.OptionMenu(lcanvas, var1, *(options), command = RocksOptionMenu_SelectionEvent).place(x=10, y=50)
+
 
 root.mainloop()
