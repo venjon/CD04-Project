f48135be4db127af3a4c8ad3381278031472497e
tharukakk1 <tharukakk1@gmail.com>
2022-07-13 16:49:47 -0700

    Update FrontEnd.py

--- FrontEnd.py
+++ FrontEnd.py
@@ -182,6 +182,8 @@ def createRockRight():
     saverock = tk.Button(rockoptionscanvas[rc],text="Save BEFORE Switching Page", command=rockSave, bg="#00ff00")
     saverock.grid(column=0,row=0,columnspan=2)
 
+rightPanels = []
+
 #Left-Side Menus
 #TITLE
 titleLabel = tk.Label(lcanvas, text="Document Title (max 80 characters):").place(x=10, y=10)
@@ -221,6 +223,7 @@ def addMaterial():
         rockList = tk.OptionMenu(lcanvas, var1, *(rockoptions), command = RocksOptionMenu_SelectionEvent).place(x=10,y=50)
         newrockscanvas = tk.Canvas(rcanvas, width=400, height=800, bg="#c1e2fe")
         rockoptionscanvas.append(newrockscanvas)
+        rightPanels.append(newrockscanvas)
         createRockRight()
     tk.Button(newMat, text="Enter", command=lambda:var3()).pack()
 
@@ -233,5 +236,144 @@ rockDict = {}
 rockList.place(x=10,y=50)
 
 #MULTI
+def multiclick():
+    global multiCanvas
+    for x in range():
+        rightPanels[x].pack_forget()
+    multiCanvas = tk.Canvas(rcanvas,width=400,height=800,bg="#b1fbbd")
+    rightPanels.append(multiCanvas)
+    multiCanvas.pack()
+    #NK
+    tk.Label(multiCanvas,text=" - NK").grid(column=0,row=1)
+    global NK
+    NK = tk.StringVar()
+    tk.Entry(multiCanvas,textvariable=NK).grid(column=1,row=1)
+    #NEQ
+    tk.Label(multiCanvas,text=" - NEQ").grid(column=0,row=2)
+    global NEQ
+    NEQ= tk.StringVar()
+    tk.Entry(multiCanvas,textvariable=NEQ).grid(column=1,row=2)
+    #NPH
+    tk.Label(multiCanvas,text=" - NPH").grid(column=0,row=3)
+    global NPH
+    NPH = tk.StringVar()
+    tk.Entry(multiCanvas,textvariable=NPH).grid(column=1,row=3)
+    #NB
+    tk.Label(multiCanvas,text=" - NB").grid(column=0,row=4)
+    global NB
+    NB = tk.StringVar()
+    tk.Entry(multiCanvas,textvariable=NB).grid(column=1,row=4)
+    #NKIN
+    tk.Label(multiCanvas,text=" - NKIN").grid(column=0,row=5)
+    global NKIN
+    NKIN = tk.StringVar()
+    tk.Entry(multiCanvas,textvariable=NKIN).grid(column=1,row=5)
+    #SAVE
+    def multiSave():
+        multi = []
+        multi = [NK.get(),NEQ.get(),NPH.get(),NB.get(),NKIN.get()]
+        print(multi)
+    global savemulti
+    savemulti = tk.Button(multiCanvas,text="Save BEFORE Switching Page", command=multiSave, bg="#00ff00")
+    savemulti.grid(column=0,row=0,columnspan=2)
+tk.Button(lcanvas, text="MULTI", command=multiclick).place(x=10,y=90)
+
+#PARAM
+def paramclick():
+    global paramCanvas
+    for x in range():
+        rightPanels[x].pack_forget()
+    paramCanvas = tk.Canvas(rcanvas,width=400,height=800,bg="#ffffc5")
+    rightPanels.append(paramCanvas)
+    paramCanvas.pack()
+    #NOITE
+    tk.Label(paramCanvas,text=" - NOITE").grid(column=0,row=1)
+    global NOITE
+    NOITE = tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=NOITE).grid(column=1,row=1)
+    #KDATA
+    tk.Label(paramCanvas,text=" - KDATA").grid(column=0,row=2)
+    global KDATA
+    KDATA= tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=KDATA).grid(column=1,row=2)
+    #MCYC
+    tk.Label(paramCanvas,text=" - MCYC").grid(column=0,row=3)
+    global MCYC
+    MCYC = tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=MCYC).grid(column=1,row=3)
+    #MESC
+    tk.Label(paramCanvas,text=" - MESC").grid(column=0,row=4)
+    global MESC
+    MESC = tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=MESC).grid(column=1,row=4)
+    #MCYPR
+    tk.Label(paramCanvas,text=" - MCYPR").grid(column=0,row=5)
+    global MCYPR
+    MCYPR = tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=MCYPR).grid(column=1,row=5)
+    #MOP (I) I=1,24
+    tk.Label(paramCanvas,text=" - MOP (I) I=1,24").grid(column=0,row=6)
+    global MOPII124
+    MOPII124 = tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=MOPII124).grid(column=1,row=6)
+    #TEXP
+    tk.Label(paramCanvas,text=" - TEXP").grid(column=0,row=7)
+    global TEXP
+    TEXP = tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=TEXP).grid(column=1,row=7)
+    #BE
+    tk.Label(paramCanvas,text=" - BE").grid(column=0,row=8)
+    global BE
+    BE = tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=BE).grid(column=1,row=8)
+    #TSTART
+    tk.Label(paramCanvas,text=" - TSTART").grid(column=0,row=9)
+    global TSTART
+    TSTART= tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=TSTART).grid(column=1,row=9)
+    #TIMAX
+    tk.Label(paramCanvas,text=" - TIMAX").grid(column=0,row=10)
+    global TIMAX
+    TIMAX= tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=TIMAX).grid(column=1,row=10)
+    #DELTEN OR NDLT
+    tk.Label(paramCanvas,text="DELTEN or NDLT?").grid(column=0,row=11)
+    global DELTENORNDLT
+    DELTENORNDLT = tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=DELTENORNDLT).grid(column=1,row=11)
+    #DELTMX
+    tk.Label(paramCanvas,text=" - DELTMX").grid(column=0,row=12)
+    global DELTMX
+    DELTMX = tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=DELTMX).grid(column=1,row=12)
+    #ELST
+    tk.Label(paramCanvas,text=" - ELST").grid(column=0,row=13)
+    global ELST
+    ELST = tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=ELST).grid(column=1,row=13)
+    #GF
+    tk.Label(paramCanvas,text=" - GF").grid(column=0,row=14)
+    global GF
+    GF = tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=GF).grid(column=1,row=14)
+    #REDLT
+    tk.Label(paramCanvas,text=" - REDLT").grid(column=0,row=15)
+    global REDLT
+    REDLT = tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=REDLT).grid(column=1,row=15)
+    #SCALE
+    tk.Label(paramCanvas,text=" - SCALE").grid(column=0,row=16)
+    global SCALE
+    SCALE = tk.StringVar()
+    tk.Entry(paramCanvas,textvariable=SCALE).grid(column=1,row=16)
+    #SAVE
+    def paramSave():
+        param = []
+        param = [NOITE.get(),KDATA.get(),MCYC.get(),MESC.get(),MCYPR.get(),MOPII124.get(),TXP.get(),BE.get(),TSTART.get(),TIMAX.get(),DELTENORNDLT.get(),DELTMX.get(),ELST.get(),GF.get(),REDLT.get(),SCALE.get()]
+        print(param)
+    global saveparam
+    saveparam = tk.Button(paramCanvas,text="Save BEFORE Switching Page", command=paramSave, bg="#00ff00")
+    saveparam.grid(column=0,row=0,columnspan=2)
+tk.Button(lcanvas, text="PARAM", command=paramclick).place(x=10,y=130)
 
 root.mainloop()
